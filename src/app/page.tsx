fc90b0e80585ffad1e304b88ff673a6a6c619580
yokohama-TN <yokohama.learning@gmail.com>
2024-01-03 23:32:08 +0900

    feat: ◯×が表示されるようになった

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,48 @@
 "use client"
 import { useState } from "react";
 
-function Square() {
-  const [changeValue, setChangeValue] = useState(null)
-  function handleClick() {
-    setChangeValue('X');
-  }
-  return <button className="square" onClick={handleClick}>{changeValue}</button>;
+type SquareProps = {
+  value: string;
+  onSquareClick: () => void;
+}
+
+function Square({ value, onSquareClick }: SquareProps) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleClick = (i: number) => {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if(xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
   return (
-    <>
-      <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+    <div className="grid grid-cols-3 gap-4">
+      <div className="border-2 border-gray-300">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+      <div className="border-2 border-gray-300">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+      <div className="border-2 border-gray-300">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-    </>
+    </div>
   )
 }
