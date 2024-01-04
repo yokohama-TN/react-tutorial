727f2f74d3d5001b36f3420bd07caad4e895840b
yokohama-TN <yokohama.learning@gmail.com>
2024-01-04 10:10:14 +0900

    feat:スタイルの設定 途中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from "react";
+import 'tailwindcss/tailwind.css';
 
 type SquareProps = {
   value: string;
@@ -7,7 +8,7 @@ type SquareProps = {
 }
 
 function Square({ value, onSquareClick }: SquareProps) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+  return <button className="border-2 border-gray-300" onClick={onSquareClick}>{value}</button>;
 }
 
 export default function Board() {
@@ -27,18 +28,18 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <div className="border-2 border-gray-300">
+    <div>
+      <div className="grid grid-cols-3 gap-4">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div className="border-2 border-gray-300">
+      <div className="grid grid-cols-3 gap-4">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div className="border-2 border-gray-300">
+      <div className="grid grid-cols-3 gap-4">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
